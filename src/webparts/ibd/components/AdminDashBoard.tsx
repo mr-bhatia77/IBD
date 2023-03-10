dbb87d5e22667e9e76473a2bd2db7e86fd97a654
Bharat Bhatia <bharat.bhatia_c@leadschool.in>
2023-03-10 16:29:37 +0530

    upload csv

--- src/webparts/ibd/components/AdminDashBoard.tsx
+++ src/webparts/ibd/components/AdminDashBoard.tsx
@@ -1,42 +1,46 @@
-import * as React from 'react';
-// import { useState } from 'react';
+import * as React from "react";
+import { useState } from "react";
 // import { Table } from 'react-bootstrap';
-import { Form, Row, Col } from 'react-bootstrap';
+import AxiosInstance from "../services/AxiosInstance";
+import { Form, Row, Col, Button } from "react-bootstrap";
 
-const projectOptions = ['Select Project','Option 1', 'Option 2', 'Option 3', 'Option 4', 'Option 5'];
+const projectOptions = [
+  "Select Project",
+  "Option 1",
+  "Option 2",
+  "Option 3",
+  "Option 4",
+  "Option 5",
+];
 
 function Dashboard() {
-  // const [data, setData] = useState([
-  //   { id: 1, sampleId:'S1', type: 'blood', checked: false },
-  //   { id: 2, sampleId:'S2', type: 'tissue', checked: false },
-  //   { id: 3, sampleId:'S3', type: 'blood', checked: false },
-  //   { id: 4, sampleId:'S4', type: 'tissue', checked: false },
-  //   { id: 5, sampleId:'S5', type: 'blood', checked: false },
-  //   { id: 6, sampleId:'S6', type: 'tissue', checked: false },
-  //   { id: 7, sampleId:'S7', type: 'blood', checked: false },
-  //   { id: 8, sampleId:'S8', type: 'tissue', checked: false },
-  // ]);
+  const [file, setFile] = useState(null);
 
-  // const handleCheckboxChange = (id: number) => {
-  //   setData(
-  //     data.map((item) => {
-  //       if (item.id === id) {
-  //         return { ...item, checked: !item.checked };
-  //       } else {
-  //         return item;
-  //       }
-  //     })
-  //   );
-  // };
+  const handleFileChange = (event: any) => {
+    setFile(event.target.files[0]);
+  };
+
+  const handleFileUpload = () => {
+    const formData = new FormData();
+    formData.append("file", file);
+
+    AxiosInstance.post("/api/upload", formData)
+      .then((response: any) => {
+        console.log(response.data);
+      })
+      .catch((error: any) => {
+        console.log(error);
+      });
+  };
 
   return (
     <>
       <Form>
-        <Row className='mb-3'>
+        <Row className="mb-3">
           <Form.Group as={Col} controlId="projectName">
             <Form.Label>Project Name:</Form.Label>
             <Form.Select>
-              {projectOptions.map(option => (
+              {projectOptions.map((option) => (
                 <option key={option}>{option}</option>
               ))}
             </Form.Select>
@@ -46,51 +50,17 @@ function Dashboard() {
             <Form.Label>Project Status:</Form.Label>
             <Form.Control type="text" disabled />
           </Form.Group>
-
-          <Form.Group as={Col} controlId="siteName">
-            <Form.Label>Site name:</Form.Label>
-            <Form.Control type="text" disabled />
-          </Form.Group>
-
         </Row>
-        <Row className='mb-3'>
-          <Form.Group as={Col} controlId="sampleType">
-            <Form.Label>Sample type:</Form.Label>
-            <Form.Select>
-              <option>Select Sample Type</option>
-              <option>blood</option>
-              <option>tissue</option>
-            </Form.Select>
-          </Form.Group>
-
-          <Form.Group as={Col} controlId="sampleCount">
-            <Form.Label>Sample count:</Form.Label>
-            <Form.Control type="text" disabled />
+        <Row>
+          <Form.Group controlId="formFile" className="mb-3">
+            <Form.Label>Select sample list to be uploaded</Form.Label>
+            <Form.Control type="file" onChange={handleFileChange} />
+            <Button className="mt-3" variant="success" onClick={handleFileUpload}>
+              Upload CSV
+            </Button>
           </Form.Group>
         </Row>
       </Form>
-      {/* <Table striped bordered hover >
-        <thead style={{ backgroundColor: 'blue', color: 'white' }}>
-          <tr>
-            <th>S.No</th>
-            <th>Sample ID</th>
-            <th>Sample Type</th>
-            <th>Assign to Project</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((item) => (
-            <tr key={item.id}>
-              <td>{item.id}</td>
-              <td>{item.sampleId}</td>
-              <td>{item.type}</td>
-              <td>
-                <input type="checkbox" checked={item.checked} onChange={() => handleCheckboxChange(item.id)} />
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </Table> */}
     </>
   );
 }

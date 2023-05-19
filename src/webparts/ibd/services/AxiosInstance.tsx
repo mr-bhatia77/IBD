bec022285eaa62d1e9ed911b14b0d6cd519e90d0
Bharat Bhatia <bharat.bhatia_c@leadschool.in>
2023-05-19 17:01:56 +0530

    axios configure

--- src/webparts/ibd/services/AxiosInstance.tsx
+++ src/webparts/ibd/services/AxiosInstance.tsx
@@ -2,10 +2,11 @@
 import axios from 'axios';
 
 const AxiosInstance = axios.create({
-    baseURL: 'http://localhost:8080/ibd'
+    baseURL: 'http://52.207.0.145:8081/ibd'
 });
 
 export default AxiosInstance;
 
 //'http://localhost:8080/tsdata'
 //'https://analytics.crohnscolitisfoundation.org/tsdata'
+// 'http://52.207.0.145:8081/ibd'
